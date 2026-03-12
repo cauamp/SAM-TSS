99f384adccd0c13b848083afedaa03a96d0ca721
cauamp <cauamp11@gmail.com>
2026-03-12 14:21:34 -0600

    fix

--- og_train.sh
+++ og_train.sh
@@ -1,7 +1,8 @@
 # Model Training
 
 # Stage1: Training Warm-up for model
-uv run   --model mvnet.py \
+uv run  src/sam_tss/main.py \
+  --model mvnet.py \
   --sam2-ckpt .ignore  \ 
 --savedir ${SAVEDIR:-save/training_base_rtmvss} \
 --backbone deeplab50 --training --baseline-mode --gpus $GPUS --lr-start 2e-4 --lr-strategy plateau_08 --num-epochs 150 --batch-size 2 --stm-queue-size 3 --sample-rate 3 --savedir save/training_base_deeplab

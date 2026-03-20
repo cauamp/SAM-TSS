2a6837e3eeb833fd658fdcb80e4677754dbd2641
cauamp <cauamp11@gmail.com>
2026-03-19 18:57:43 -0600

    Add rtmvss_7a model implementation with frame/video queries per class

--- src/sam_tss/utils/loading.py
+++ src/sam_tss/utils/loading.py
@@ -16,6 +16,8 @@ from models.rtmvss_40 import rtmvss as rtmvss4
 from models.rtmvss_5 import rtmvss as rtmvss5
 from models.rtmvss_6a import build_rtmvss6_from_args as build_rtmvss6_from_argsa
 from models.rtmvss_7 import rtmvss as rtmvss7
+from models.rtmvss_7a import rtmvss as rtmvss7a
+
 models = {
     "mvnet": MVNet,
     "rtmvss_1": rtmvss,
@@ -32,6 +34,8 @@ models = {
     "rtmvss_5": rtmvss5,
     "rtmvss_6a": build_rtmvss6_from_argsa,
     "rtmvss_7": rtmvss7,
+    "rtmvss_7a": rtmvss7a
+
 }
 
 
